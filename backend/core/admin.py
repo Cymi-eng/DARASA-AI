279841aba6d0ce0b3bd4527f19a65516921dac84
cymi-eng <eaiancymi@gmail.com>
2026-09-10 10:04:43 +0300

    feat: Registered Competency in Django admin with filters and search

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Competency
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('admission_number', 'first_name', 'last_name', 'grade', 'guardian_phone')
     list_filter = ('grade',)
     search_fields = ('first_name', 'last_name', 'admission_number')
+
+
+@admin.register(Competency)
+class CompetencyAdmin(admin.ModelAdmin):
+    list_display = ('student', 'learning_area', 'strand', 'mastery_level', 'assessed_on')
+    list_filter = ('learning_area', 'mastery_level', 'assessed_on')
+    search_fields = ('student__first_name', 'student__last_name', 'student__admission_number', 'strand')
