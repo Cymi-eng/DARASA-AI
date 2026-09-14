2f660b10140d0850511d6cbaa22e53390f472e39
cymi-eng <eaiancymi@gmail.com>
2026-09-14 20:27:20 +0300

    feat(api): configure JWT and REST API routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,77 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .views import (
+    StudentViewSet,
+    CompetencyViewSet,
+    SchoolViewSet,
+    ClassRoomViewSet,
+    TeacherViewSet,
+    FeePaymentViewSet,
+    DashboardViewSet,
+)
+
+
+router = DefaultRouter()
+
+router.register(
+    r"students",
+    StudentViewSet,
+)
+
+router.register(
+    r"competencies",
+    CompetencyViewSet,
+)
+
+router.register(
+    r"schools",
+    SchoolViewSet,
+)
+
+router.register(
+    r"classrooms",
+    ClassRoomViewSet,
+)
+
+router.register(
+    r"teachers",
+    TeacherViewSet,
+)
+
+router.register(
+    r"fee-payments",
+    FeePaymentViewSet,
+)
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("core.urls")),
+    path(
+        "auth/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+
+    path(
+        "auth/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+
+    path(
+        "dashboard/",
+        DashboardViewSet.as_view(
+            {
+                "get": "list"
+            }
+        ),
+        name="dashboard",
+    ),
 ]
+
+
+urlpatterns += router.urls
