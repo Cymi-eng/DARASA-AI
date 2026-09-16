97f0f9174a390a61d1878f7bebb5f6dfc180c2d7
cymi-eng <eaiancymi@gmail.com>
2026-09-16 14:30:07 +0300

    fix(api): mount DRF router with include

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
@@ -107,6 +107,6 @@ urlpatterns = [
 
     path(
         "api/",
-        router.urls,
+        include(router.urls),
     ),
 ]
