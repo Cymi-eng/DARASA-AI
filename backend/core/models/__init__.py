3ec740fb7bb892e313d0983d9dcabd88784399e2
cymi-eng <eaiancymi@gmail.com>
2026-09-16 12:44:58 +0300

    refactor(models): export separated model classes

---
+++ backend/core/models/__init__.py
@@ -0,0 +1,17 @@
+from .school import School
+from .user_profile import UserProfile
+from .student import Student
+from .classroom import ClassRoom
+from .teacher import Teacher
+from .competency import Competency
+from .fee_payment import FeePayment
+
+__all__ = [
+    "School",
+    "UserProfile",
+    "Student",
+    "ClassRoom",
+    "Teacher",
+    "Competency",
+    "FeePayment",
+]
